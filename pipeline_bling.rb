d3cbc80eb374c0f175e7af3d65206d4a9ede6d49
Moshe Bildner <moshe.bildner@gmail.com>
2017-05-01 21:19:24 -0400

    pull apart some methods

--- pipeline_bling.rb
+++ pipeline_bling.rb
@@ -2,8 +2,10 @@ require 'yaml'
 require 'json'
 
 class PipelineScanner
-  def initialize(pipeline_name)
+  def initialize(pipeline_name, pipeline_path, fly_target)
     @pipeline_name = pipeline_name
+    @pipeline_path = pipeline_path
+    @fly_target = fly_target
   end
 
   def scan_for_leaks
@@ -12,47 +14,61 @@ class PipelineScanner
       run_id = last_successful_run(job_name)
       log = logs_for_build(job_name, run_id)
       notes.map do |note|
-        note['note'].map do |k, v|
+        note[:note].map do |k, v|
           if log.include?(v)
             {
-              "pipeline" => pipeline_name,
-              "job" => job_name,
-              "build" => run_id,
-              "key" => k,
-              "note" => note['name']
+                pipeline: pipeline_name,
+                job: job_name,
+                build: run_id,
+                key: k,
+                note: note[:name]
             }
           end
         end
       end
     end.flatten.compact
   end
+
   private
 
-  attr_reader :pipeline_name
+  attr_reader :pipeline_name, :pipeline_path, :fly_target
 
   def pipelines
-    `fly -t pipeline-bling pipelines --all`.split("\n").map{|l| l.split(/\s/)[0] }
+    `fly -t #{fly_target} pipelines --all`.split("\n").map {|l| l.split(/\s/)[0]}
   end
 
   def lpass_notes
-    File.read('pipeline.yml')
-      .scan(/\({2}(.+)\){2}/)
-      .map(&:first)
-      .map{|full_note_path| full_note_path.split('/Notes/')[0]}
-      .map{|note_name| { "name" => note_name, "note" => YAML.load(`lpass show --notes "#{note_name}"`) }}
+    File.read(pipeline_path)
+        .scan(/\({2}(.+)\){2}/)
+        .map(&:first)
+        .map {|full_note_path| full_note_path.split('/Notes/')[0]}
+        .map {|note_name| { name: note_name, note: YAML.safe_load(`lpass show --notes "#{note_name}"`)}}
   end
 
   def jobs
-    YAML.load(`fly -t pipeline-bling get-pipeline -p #{pipeline_name}`)['jobs'].map{|job| job['name'] }
+    pipeline(pipeline_name).fetch('jobs').map { |job| job['name'] }
+  end
+
+  def pipeline(name)
+    YAML.safe_load(`fly -t #{fly_target} get-pipeline -p #{name}`)
   end
 
   def last_successful_run(job_name)
-    `fly -t pipeline-bling builds -j #{pipeline_name}/#{job_name}`.split("\n").find{|job| job.split(/\s+/)[3] == 'succeeded' }[/\d+/].to_i
+    builds(job_name).find {|job| job.split(/\s+/)[3] == 'succeeded'}[/\d+/].to_i
+  end
+
+  def builds(job_name)
+    `fly -t #{fly_target} builds -j #{pipeline_name}/#{job_name}`.split("\n")
   end
 
   def logs_for_build(job_name, build)
-    `fly -t pipeline-bling watch -j #{pipeline_name}/#{job_name} -b #{build}`
+    `fly -t #{fly_target} watch -j #{pipeline_name}/#{job_name} -b #{build}`
   end
 end
 
-puts PipelineScanner.new('hello-world').scan_for_leaks.to_json
+pipeline_name = 'hello-world'
+pipeline_path = 'pipeline.yml'
+fly_target = 'pipeline-bling'
+
+
+puts PipelineScanner.new(pipeline_name, pipeline_path, fly_target).scan_for_leaks.to_json
